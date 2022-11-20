c861026c2c6eaabf66fe46b918ecccde7f429ce3
国进 颜 <guojin_yjs@cumt.edu.cn>
2022-11-20 19:26:54 +0800

    添加项目文件。

---
+++ model_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace model_test // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
