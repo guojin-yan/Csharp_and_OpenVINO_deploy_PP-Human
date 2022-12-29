30ae9144903b735f6de5c9bfeae011ce460fad9f
国进 颜 <guojin_yjs@cumt.edu.cn>
2022-12-29 14:41:39 +0800

    主函数

--- ProjectTest/Program.cs
+++ ProjectTest/Program.cs
@@ -9,6 +9,7 @@ namespace ProjectTest // Note: actual namespace depends on the project name.
             Console.WriteLine("Hello World!");
 
             HumanFallDown.human_fall_down();
+
         }
     }
 }
