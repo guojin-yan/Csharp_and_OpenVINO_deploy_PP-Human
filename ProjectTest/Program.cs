8082bebadd56ca3103d96a3dbe1107f87dcc06c5
国进 颜 <guojin_yjs@cumt.edu.cn>
2023-01-02 12:24:56 +0800

    完善项目

--- ProjectTest/Program.cs
+++ ProjectTest/Program.cs
@@ -6,7 +6,7 @@ namespace ProjectTest // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
 
             HumanFallDown.human_fall_down();
 
