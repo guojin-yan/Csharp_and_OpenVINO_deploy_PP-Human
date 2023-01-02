c26213f248657d276dd72fdc72396a9146a73c52
国进 颜 <guojin_yjs@cumt.edu.cn>
2023-01-02 12:24:27 +0800

    时间测试

---
+++ ModelTimeTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ModelTimeTest // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+
+            //PP_YOLOE yoloe = new PP_YOLOE();
+            //yoloe.test_time();
+
+
+
+            //PP_TinyPose pose = new PP_TinyPose();
+            //pose.test_time();
+
+
+            STGCN sTGCN = new STGCN();
+            sTGCN.test_time();
+        }
+    }
+}
